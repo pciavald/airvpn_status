d4cdce2b090cca3ff47a5112937dc55636dd1350
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-08-29 01:30:31 +0200

    sudo

--- py3status_install.sh
+++ py3status_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DIR=`sudo find / -type d -name py3status`/modules
-cp airvpn_status.py $DIR
-cp airvpn_api.key $DIR
-cp airvpn_i3.py $DIR/airvpn.py
+sudo cp airvpn_status.py $DIR
+sudo cp airvpn_api.key $DIR
+sudo cp airvpn_i3.py $DIR/airvpn.py
